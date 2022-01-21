6b6a073c7a3f2f81f9b366ac820d9dd5335a7fc2
heyjin281129 <hyejin281129@gmail.com>
2022-01-21 15:16:39 +0900

    질문 목록 보여주기

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,8 @@
-from flask import Blueprint
+# blueprints : 라우트 함수 관리
+# render_template : 템플릿 파일을 화면으로 렌더링
+from flask import Blueprint, blueprints, render_template
+
+from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -8,4 +12,6 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-  return 'Pybo index'
+  question_list = Question.query.order_by(Question.create_date.desc())
+  return render_template('question/question_list.html', question_list=question_list)
+
