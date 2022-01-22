afc06243c1007206575b222a95804a299ec65bc8
heyjin281129 <hyejin281129@gmail.com>
2022-01-22 16:31:48 +0900

    답글 댓글 기능

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -25,7 +25,9 @@ def create(question_id):
         answer = Answer(content=content, create_date=datetime.now(), user=g.user)
         question.answer_set.append(answer)
         db.session.commit()
-        return redirect(url_for('question.detail', question_id=question_id))
+        return redirect('{}#answer_{}'.format(
+            url_for('question.detail', question_id=question_id), answer.id)
+            )
     return render_template('question/question_detail.html', question=question, form=form)
 
 # 답변 수정 modify
@@ -42,7 +44,9 @@ def modify(answer_id):
             form.populate_obj(answer)
             answer.modify_date = datetime.now()  # 수정일시 저장
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=answer.question.id))
+            return redirect('{}#answer_{}'.format(
+                url_for('question.detail', question_id=answer.question.id), answer.id)
+            )
     else:
         form = AnswerForm(obj=answer)
     return render_template('answer/answer_form.html', form=form)
