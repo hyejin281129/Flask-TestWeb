9a66d5dae7d03236e85f1f39d7643c4ed7daaeb0
heyjin281129 <hyejin281129@gmail.com>
2022-01-22 12:47:50 +0900

    페이징완료!

--- pybo/forms.py
+++ pybo/forms.py
@@ -2,6 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
+# 내용등록폼
 class QuestionForm(FlaskForm):
-    subject = StringField('제목', validators=[DataRequired()])
-    content = TextAreaField('내용', validators=[DataRequired()])
+    subject = StringField('제목', validators=[DataRequired('제목을 입력해주세요')])
+    content = TextAreaField('내용', validators=[DataRequired('내용을 입력해주세요')])
+
+# 답변등록폼
+class AnswerForm(FlaskForm):
+    content = TextAreaField('내용', validators=[DataRequired('내용을 입력해주세요')])
