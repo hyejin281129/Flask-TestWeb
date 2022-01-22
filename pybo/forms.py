a0236ce11d3f2d554517c6cbdd2a5a25ac4b287e
heyjin281129 <hyejin281129@gmail.com>
2022-01-22 13:41:43 +0900

    로그인/로그아웃 구현

--- pybo/forms.py
+++ pybo/forms.py
@@ -11,11 +11,16 @@ class QuestionForm(FlaskForm):
 class AnswerForm(FlaskForm):
     content = TextAreaField('내용', validators=[DataRequired('내용을 입력해주세요')])
 
-# 로그인폼
+# 회원가입 폼
 class UserCreateForm(FlaskForm):
-    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    username = StringField('사용자이름', validators=[DataRequired('사용자의 이름을 입력해주세요'), Length(min=3, max=25)])
     password1 = PasswordField('비밀번호', validators=[
-        DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다')
+        DataRequired('비밀번호를 입력해주세요'), EqualTo('password2', '비밀번호가 일치하지 않습니다')
     ])
-    password2 = PasswordField('비밀번호확인', validators=[DataRequired()])
-    email = EmailField('이메일', validators=[DataRequired(), Email()])
+    password2 = PasswordField('비밀번호확인', validators=[DataRequired('비밀번호 확인을 입력해주세요')])
+    email = EmailField('이메일', validators=[DataRequired('이메일을 입력해주세요'), Email()])
+
+# 로그인 폼
+class UserLoginForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired('사용자의 이름을 입력해주세요'), Length(min=3, max=25)])
+    password = PasswordField('비밀번호', validators=[DataRequired('비밀번호를 입력해주세요')])
