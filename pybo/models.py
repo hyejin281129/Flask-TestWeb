e02bcc717382a20227b5c6cd52094b00d4be2942
heyjin281129 <hyejin281129@gmail.com>
2022-01-22 13:56:43 +0900

    SQLite 설정 수정

--- pybo/models.py
+++ pybo/models.py
@@ -6,6 +6,10 @@ class Question(db.Model):
   subject = db.Column(db.String(200), nullable=False)
   content = db.Column(db.Text(), nullable=False)
   create_date = db.Column(db.DateTime(), nullable=False)
+  # User 모델 데이터의 id 값을 Question 모델에 포함시킴
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+  # Question 모델에서 User모델을 참조함
+  user = db.relationship('User', backref=db.backref('question_set'))
 
 # Answer 클래스
 class Answer(db.Model):
